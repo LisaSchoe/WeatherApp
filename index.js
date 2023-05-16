953a2a83aca4da4e75aae0e2b27e02cff2a2beb0
LisaSchoe <lisa2391@gmx.at>
2023-05-16 10:56:33 +0200

    Added forecast heading and forecast loop

--- index.js
+++ index.js
@@ -71,13 +71,13 @@ function handleSubmit(event) {
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  
-  let forecastHTML= `<div class="row w-100">`;
-  let days = ["Thu","Fri","Sat"];
+    let days = ["Thu","Fri","Sat","Sun"];
+
+  let forecastHTML= `<div class="row">`;
   days.forEach(function(day){
   forecastHTML = forecastHTML + `
         <div class="col-2">
-      <div class="weather-forecast-date">Monday</div>
+      <div class="weather-forecast-date">${day}</div>
       <img src="☀"
       alt=""
       width=""/>
@@ -85,8 +85,7 @@ function displayForecast() {
         <span class="weather-forecast-temperature-max"> 18° </span>
         <span class="weather-forecast-temperature-min"> 15° </span>
       </div>
-    </div>
-  </div>`;
+    </div>`;
   })
 
   forecastHTML=forecastHTML + `</div>`;

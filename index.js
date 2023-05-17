f5104c8ffbfcaf48689e31a478a13126a08a07aa
LisaSchoe <lisa2391@gmx.at>
2023-05-17 10:41:46 +0200

    finished the forecast api integration for 6 days

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ function formatDate(timestamp){
 return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+let date= new Date (timestamp*1000);
+let day=date.getDay();
+let days= ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+return days[day];
+}
+
 function searchEngine(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
@@ -80,23 +87,25 @@ function handleSubmit(event) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast=response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-    let days = ["Thu","Fri","Sat","Sun"];
+  
+  //let days = ["Thu","Fri","Sat","Sun"];
 
   let forecastHTML= `<div class="row">`;
-  days.forEach(function(day){
+  forecast.forEach(function(forecastDay, index){
+    if (index < 6){
   forecastHTML = forecastHTML + `
         <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <img src="☀"
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
       alt=""
       width=""/>
       <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-max"> 18° </span>
-        <span class="weather-forecast-temperature-min"> 15° </span>
+        <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+        <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
       </div>
-    </div>`;
+    </div>`;}
   })
 
   forecastHTML=forecastHTML + `</div>`;
